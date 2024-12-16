756456cce1e497f764ba8811d0e5454656905b8e
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2024-12-16 20:59:59 +0700

    feat: make migration

---
+++ database/migrations/2024_12_16_135502_create_cagar_budaya_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cagar_budaya', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("jenis_id")->constrained()->onUpdate("cascade")->onDelete("cascade");
+            $table->foreignId("status_id")->constrained("status")->onUpdate("cascade")->onDelete("cascade");
+            $table->string("nama_objek");
+            $table->string("sk_penetapan");
+            $table->string("desa_kelurahan");
+            $table->string("kecamatan");
+            $table->string("kabupaten_kota");
+            $table->string("provinsi");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cagar_budaya');
+    }
+};
